4bab0bd3c07e44ce00a9181003a37b299ad8abd4
mbuvi254 <danielmulatya95@gmail.com>
2025-11-20 01:09:43 +0300

    Updated Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Spinner } from "./components/ui/spinner";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardTitle,
-  CardDescription,
-} from "./components/ui/card";
-
-import { Input } from "./components/ui/input";
-import { Button } from "./components/ui/button";
-import {useState} from 'react'
+import SearchForm from "./components/SearchForm";
+// import { SearchResultCard } from "./components/SearchResultCard";
 
 
 
 function App() {
-  const [wordSearch,setWordSearch]=useState("");
+  return(
+    <SearchForm />
 
 
-  const { data, isLoading, error,refetch } = useQuery({
-    queryKey: ["get-word-meaning"],
-    queryFn: async () => {
-      const res = await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${wordSearch}`,
-      );
-      console.log(res.data);
-      return res.data;
-    },
-    enabled : false,
-  });
-
- if (isLoading) {
-    return (
-      <div className="flex flex-col items-center gap-4 mt-12">
-        <Spinner className="size-20" />
-        <p className="text-gray-600 text-lg">
-          Please Wait, data is still loading...
-        </p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return <p>Something went wrong</p>;
-  }
-
-const handleSubmit = (e:any) => {
-    e.preventDefault();
-
-    if (!wordSearch.trim()) {
-      alert(`Please Provide a Word`);
-    }
-    refetch();
-  
-};
-
-  return (
-    <>
-      <h1 className="text-2xl font-bold mb-4">WORD RESULT</h1>
-
-       <form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          onChange={(e) => setWordSearch(e.target.value)}
-          placeholder="Enter Word to Search"
-        />
-        <Button type="submit">Search</Button>
-      </form>
-
-      {data?.map((item: any, index: number) => (
-        <Card key={index} className="mb-6">
-          <CardHeader>
-            <CardTitle>{item.word}</CardTitle>
-            <CardDescription>
-              {item.phonetic ?? "No phonetic available"}
-            </CardDescription>
-          </CardHeader>
-
-          <CardContent>
-            {item.meanings.map((meaning: any, mIndex: number) => (
-              <div key={mIndex} className="mb-4">
-                <p className="font-semibold">
-                  Parts of Speech: {meaning.partOfSpeech}
-                </p>
-                {meaning.definitions.map((definition: any, dIndex: number) => (
-                  <p key={dIndex} className="text-gray-700">
-                    - {definition.definition}
-                  </p>
-                ))}
-              </div>
-            ))}
-          </CardContent>
-        </Card>
-      ))}
-    </>
-  );
+  )
+ 
 }
 
 
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,27 +1,90 @@
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import {useState} from "react"
+import {useState} from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Spinner } from "./ui/spinner";
+import { useSearchResultStore} from "../store/SearchStore"
+import SearchResultCard from "./SearchResultCard";
 
-const [wordSearch, setWordSearch]=useState("");
-const handleSubmit = (e:any) => {
+
+const SearchForm = ()=>{
+const [wordSearch,setWordSearch]=useState("");
+const setResults = useSearchResultStore(state=>state.setResults);
+     
+  const { data, isLoading, error,refetch } = useQuery({
+    queryKey: ["get-word-meaning",wordSearch],
+    queryFn: async () => {
+      const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordSearch}`,);
+      console.log(res.data);
+      setResults(res.data);
+      return res.data;
+    },
+    enabled : false,
+  });
+
+ if (isLoading) {
+    return (
+      <div className="flex flex-col items-center gap-4 mt-12">
+        <Spinner className="size-20" />
+        <p className="text-gray-600 text-lg">
+          Please Wait, data is still loading...
+        </p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p>Something went wrong</p>;
+  }
+
+
+
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!wordSearch.trim()) {
       alert(`Please Provide a Word`);
+      return
     }
-    //refetch();
+    refetch();
   
 };
-const SearchForm = ()=>{
-    return (
+  return(
+    <>
+      <h1 className="text-2xl font-bold mb-4">WORD RESULT</h1>
+
+       <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          value={wordSearch}
+          onChange={(e) => setWordSearch(e.target.value)}
+          placeholder="Enter Word to Search"
+        />
+        <Button type="submit">Search</Button>
+      </form>
+
+      {isLoading && (
+         <div className="flex flex-col items-center gap-4 mt-12">
+        <Spinner className="size-20" />
+        <p className="text-gray-600 text-lg">
+          Please Wait, data is still loading...
+        </p>
+      </div>
+      )}
+      {error && (
         <>
-        <form onSubmit={handleSubmit}>
-        <Input type="text" onChange={(e)=>setWordSearch(e.target.value)} placeholder="Enter Word to Search" />
-        <Button type="submit">Search</Button> 
-        </form>
+        <p>Something Went Wrong</p>
         </>
-       
-    )
+      )}
+
+         <SearchResultCard />
+
+      
+
+      
+    </>
+  );
 }
 
 export default SearchForm;
